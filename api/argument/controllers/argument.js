a013fb33c0055dca11d3177d9b424df062516b3d
Momen <momen@afnan-tech.com>
2020-07-20 00:15:36 +0200

    add create in argument controller

--- api/argument/controllers/argument.js
+++ api/argument/controllers/argument.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
@@ -6,19 +6,52 @@
  */
 
 module.exports = {
-//    find: ctx => {
-//        return strapi.query('argument').find(ctx.query, [
-//            'reasonings','reasonings.premises'
-//        ]);
-//      },
-find: ctx => {
+  //    find: ctx => {
+  //        return strapi.query('argument').find(ctx.query, [
+  //            'reasonings','reasonings.premises'
+  //        ]);
+  //      },
+  find: ctx => {
     return strapi.query('argument').find(ctx.query, [
       {
         path: 'reasonings',
         populate: {
-          path: 'premises',
-        },
-      },
-    ]);
+          path: 'premises'
+        }
+      }
+    ])
   },
-};
+
+  create: async ctx => {
+    const { reasonings, ...argument } = ctx.request.body
+    console.log({ reasonings, argument })
+
+    
+    const savedReason = await strapi
+    .query('Reasoning').create()
+    // .create({
+    //   premise: { argument: savedArgument.id, reasoning: reasonings }
+    // })
+    // console.log({ savedReason })
+    console.log(argument)
+
+    const a = {
+      ...argument,
+      reasonings: [savedReason.id]
+    }
+    // console.log(a)
+    const savedArgument = await strapi.query('Argument').create(a)
+    await strapi.query('reasoning').update({ _id: savedReason.id }, { premises: [savedArgument.id]})
+    // console.log(savedArgument)
+
+    for (const reason of reasonings) {
+      const { statement, ...pemises } = reason
+      // console.log(statement)
+
+      const savedPremise = await strapi.query('Argument').create({ statement })
+      // console.log('saved', savedPremise)
+    }
+
+    ctx.response.send('done')
+  }
+}
