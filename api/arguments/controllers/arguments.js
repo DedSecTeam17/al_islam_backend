da4275f2dfd23ede5aa2b4e72a3872197a95b90f
m.elamin <m.elamin@afnan-tech.com>
2020-08-19 22:11:05 +0200

    init

--- api/arguments/controllers/arguments.js
+++ api/arguments/controllers/arguments.js
@@ -4,6 +4,7 @@
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
+//git commit
 
 module.exports = {
   //    find: ctx => {
